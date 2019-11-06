1b6ede1c293250f1d34c7c0e840591e5d9878050
baldurben <bab42@hi.is>
2019-11-06 00:29:49 +0000

    vinna vinna

--- scripts.js
+++ scripts.js
@@ -10,30 +10,90 @@ document.addEventListener('DOMContentLoaded', () => {
 const text = (() => {
   let items;
 
+
+  
+  
   function init(_form, _items) {
     items = _items;
     _form.addEventListener('submit', formHandler);
-	document.getElementById("box").addEventListener('change', finish(e));
+	var checkboxes = document.getElementsByClassName("item__checkbox");
+	for(var i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener('change', finish);
+	}
+	var texti = document.getElementsByClassName("item__text")
+	for(var i = 0; i < texti.length; i++) {
+		texti[i].addEventListener('click', edit);
+	}
+	var eyðatakkar = document.getElementsByClassName("item__button")
+	for(var i = 0; i < eyðatakkar.length; i++) {
+		eyðatakkar[i].addEventListener('click', deleteItem);
+	}
+	var bætavið = document.getElementsByClassName("form__button")
+	
 
     // TODO láta hluti í _items virka
   }
 
   function formHandler(e) {
     e.preventDefault();
+	var takki = document.createElement("Button")
+	takki.innerHTML = 'Eyða';
+	takki.className = "item__button";
+	takki.addEventListener('click', deleteItem);
+	var textinn = document.createElement("span")
+    textinn.innerHTML = document.getElementsByClassName("form__input")[0].value;
+	textinn.className = "item__text";
+	textinn.addEventListener('click', edit);
+	var checkkassi = document.createElement("input")
+	checkkassi.setAttribute("type", "checkbox")
+	checkkassi.className = "item__checkbox"
+	checkkassi.addEventListener('change', finish);
+	var listi = document.createElement("li")
+	listi.className = "item"
+	listi.appendChild(checkkassi);
+	listi.appendChild(textinn);
+	listi.appendChild(takki);
+	console.log(listi);
+	if(textinn.innerHTML.trim() == '' || textinn.innerHTML == null) return;
+	else document.querySelector(".items").appendChild(listi);
+	document.getElementsByClassName("form__input")[0].value = '';
+	
 
     console.log('halló heimur');
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
-	var check = document.getElementsById("box").checked;
-	if(check == true) {
-		alert();
-	}
-  }
-
+		if(this.checked == true) {	
+			Parent = this.parentNode;	
+			Parent.className += " item--done"
+		}
+		if(this.checked == false) {
+			Parent = this.parentNode;
+			Parent.className = "item"
+		}
+  }			
   // event handler fyrir það að breyta færslu
   function edit(e) {
+	/*
+	þetta = this;
+	þetta.contentEditable = true;
+	document.execCommand("defaultParagraphSeparator", false);
+	þetta.style.border='3px dotted black';
+	onmouseout = function() {
+		þetta.style.border='none';
+	}
+	*/
+	this.removeEventListener('click', edit);
+	var texti = this.innerHTML;
+	this.innerHTML = "<input type='focus' id='tex' name='type' value='"+texti+"'>";
+	this.addEventListener('keypress', function(e) {
+	if(event.keyCode === 13) {
+		this.innerHTML = document.getElementById('tex').value;
+		this.addEventListener('click', edit);
+	}	
+	}); 
+	
   }
 
   // event handler fyrir það að klára að breyta færslu
@@ -42,18 +102,14 @@ const text = (() => {
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
-	var li = document.createElement('li');
-	var chkbox = document.createElement('input');
-	var span = document.createElement('span');
-	chkbox.type = 'checkbox';
-	span.appendChild(document.createTextNode(text));
-	li.appendChild(chkbox);
-	li.appendChild(span);
-	return li;
+	  alert();
+	/*
+	*/
   }
 
   // event handler til að eyða færslu
   function deleteItem(e) {
+	  this.parentNode.parentNode.removeChild(this.parentNode);
   }
 
   // hjálparfall til að útbúa element
